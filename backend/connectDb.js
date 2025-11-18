86f0c8c9ebe8adf717c23f4ddfb0a574e82fd437
Tanupreet Kaur <tanupreetk24@gmail.com>
2025-11-18 23:38:25 +0530

    edited files

--- backend/connectDb.js
+++ backend/connectDb.js
@@ -1,15 +1,24 @@
-const mongoose = require('mongoose');
-const { ENV_VARS } = require('./config/envVar.js');
+const mongoose = require("mongoose");
+const { ENV_VARS } = require("./config/envVar.js");
 
 const mongoURI = ENV_VARS.MONGODB_URI;
 
 const connectToMongo = async () => {
-    try {
-        await mongoose.connect(mongoURI);
-        console.log('Connected to MongoDB Successfully');
-    } catch (error) {
-        console.error('Error connecting to MongoDB', error);
+  try {
+    if (!mongoURI) {
+      throw new Error("MongoDB URI is missing from ENV");
     }
-}
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectToMongo;
